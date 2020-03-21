dc3818ab55f9ed26fe35c82f3db2720a17f3c431
LuthfilK <62377847+LuthfilK@users.noreply.github.com>
2020-03-21 07:22:53 +0700

    Add files via upload

---
+++ Luthfil/Aplikasi.java
@@ -0,0 +1,12 @@
+import Luthfil;
+
+import Luthfil.Mahasiswa;
+public class Aplikasi{
+
+    public static void main(String[] args) {
+
+        Mahasiswa mhs = new Mahasiswa("18090123", "Luthfil Khkim", "4C");
+        System.out.println(mhs.info());
+        
+    }
+}
